d61050834a443c1fa8fb35c9be1acfab6fcbaf37
sunil-nvoyager <sunil.nvoyager@gmail.com>
2024-08-23 15:09:39 +0530

    Update Pizza.java

--- factory/src/main/java/product_client_impl/Pizza.java
+++ factory/src/main/java/product_client_impl/Pizza.java
@@ -12,6 +12,7 @@ abstract class Pizza {
     private ArrayList<String> toppings = new ArrayList<String>();
 
     void Pizza() {
+        
         System.out.println("Preparing " + name);
         System.out.println("Tossing dough " + dough);
         System.out.println("Adding sauce " + sauce);
@@ -19,23 +20,32 @@ abstract class Pizza {
         for (String topping : toppings) {
             System.out.println(" " + topping);
         }
+        
     }
 
     protected abstract void prepare();
 
     protected void bake() {
+        
         System.out.println("Bake for 25 minutes at 350");
+        
     }
 
     protected void cut() {
+        
         System.out.println("Cutting the pizza into diagonal slices");
+        
     }
 
     protected void box() {
+        
         System.out.println("Place pizza in official pizzastore box");
+        
     }
 
     protected String getName() {
+        
         return name;
+        
     }
 }
